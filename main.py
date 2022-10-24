0084c9eb95bf2a5b9492872a2eb2099b3fd7a2dc
wklein1 <Witali@>
2022-10-24 16:10:19 +0200

    #3 add deta db integration

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from fastapi import FastAPI
+from deta import Deta, Base
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
 
+PROJECT_KEY = config("PROJECT_KEY")
+
+deta = Deta(PROJECT_KEY)
+productsDB = deta.Base("products")
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"])
