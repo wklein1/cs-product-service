0dc41918744afc024b0a29051e268064fdbe4855
wklein1 <Witali@>
2022-10-27 21:55:03 +0200

    #9 add delete product endpoint test

--- tests/test_main.py
+++ tests/test_main.py
@@ -28,4 +28,19 @@ def test_post_products_endpoint():
     }
     response = client.post("/products",json=test_product)
     assert response.status_code == 201
-    assert test_product.items() <= response.json().items()
+    assert test_product.items() <= response.json().items
+
+def test_delete_product_endpoint():
+    client = TestClient(app)
+    TEST_USER_ID = config("TEST_USER_ID")
+    test_product = {
+        "ownerId":TEST_USER_ID,
+        "name":"test new product",
+        "componentIds":["546c08d7-539d-11ed-a980-cd9f67f7363d","546c08da-539d-11ed-a980-cd9f67f7363d"],
+        "description":"new product from post request",
+        "price":0.0
+    }
+    post_response = client.post("/products",json=test_product)
+    product_id = post_response.json()["productId"]
+    del_response = client.delete(f"/products/{product_id}",headers={"userId":TEST_USER_ID})
+    assert del_response.status_code == 204
